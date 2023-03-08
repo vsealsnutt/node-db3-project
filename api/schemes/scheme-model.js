bc47da84559ef5902d20fcb1a4c99f448d384b29
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-03-07 19:19:13 -0500

    wrote the find model function

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,4 +1,6 @@
-function find() { // EXERCISE A
+const db = require('../../data/db-config');
+
+async function find() { // EXERCISE A
   /*
     1A- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`.
     What happens if we change from a LEFT join to an INNER join?
@@ -15,6 +17,13 @@ function find() { // EXERCISE A
     2A- When you have a grasp on the query go ahead and build it in Knex.
     Return from this function the resulting dataset.
   */
+  const rows = await db('schemes as sc')
+    .select('sc.*')
+    .count('st.step_id as number_of_steps')
+    .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+    .groupBy('sc.scheme_id')
+  
+  return rows;
 }
 
 function findById(scheme_id) { // EXERCISE B
@@ -83,6 +92,8 @@ function findById(scheme_id) { // EXERCISE B
         "steps": []
       }
   */
+
+
 }
 
 function findSteps(scheme_id) { // EXERCISE C
